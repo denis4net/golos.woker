380d67238fc2daec039c2a7e34a7d5aad4d92d5f
Denis Vashchuk <vashchuk.denis@gmail.com>
2018-10-20 16:17:00 +0300

    Refactor functional tests

--- tests/golos.worker.test.js
+++ tests/golos.worker.test.js
@@ -1,99 +1,117 @@
-const EOSTest = require("eosio.test")
+const EOSTest = require("eosio.test");
 
-const eosTest = new EOSTest()
+const eosTest = new EOSTest();
+const appName = "app.sample";
+const tokenSymbol = "APP";
+
+const delegateAccounts = [];
+const memberAccounts = [];
+
+for (let i = 1; i < 22; i++) {
+  const code = String.fromCharCode('a'.charCodeAt(0) + i)
+  delegateAccounts.push(`delegate.${code}`)
+  memberAccounts.push(`user.${code}`)
+}
+
+let tokenContract = null;
+let contract = null;
 
 beforeAll(async done => {
-  console.log("init")
-  await eosTest.init()
-  done()
-})
+  console.log("init");
+  await eosTest.init();
+
+  await eosTest.newAccount(
+    ...memberAccounts,
+    ...delegateAccounts,
+    "golos.worker",
+    "golos.token",
+    appName
+  );
+
+  console.log("build & deploy token contract");
+  await eosTest.make("contracts/golos.token");
+  tokenContract = await eosTest.deploy(
+    "golos.token",
+    "contracts/golos.token/golos.token.wasm",
+    "contracts/golos.token/golos.token.abi"
+  );
+
+  console.log("build contract");
+  await eosTest.make("contracts/golos.worker");
+
+  console.log("create app token");
+  await tokenContract.create(appName, `1000000000 ${tokenSymbol}`, {
+    authorization: "golos.token"
+  });
+  await tokenContract.issue(appName, `1000000000 ${tokenSymbol}`, {
+    authorization: appName
+  });
+
+  console.log("deploy golos.worker contract");
+  contract = await eosTest.deploy(
+    "golos.worker",
+    "contracts/golos.worker/golos.worker.wasm",
+    "contracts/golos.worker/golos.worker.abi"
+  );
+
+  done();
+}, 120000);
 
 it(
-  "tests golos.worker contract",
+  "1st use case test",
   async done => {
-    const appName = "app.sample"
-    const tokenSymbol = "APP"
+    console.log("create a workers pool");
+    await contract.createpool(appName, tokenSymbol, { authorization: appName });
 
-    await eosTest.newAccount("user1", "delegate1", "delegate2", "golos.worker", "golos.token", appName)
-
-    console.log("build & deploy token contract")
-    await eosTest.make("contracts/golos.token")
-    const tokenContract = await eosTest.deploy(
-      "golos.token",
-      "contracts/golos.token/golos.token.wasm",
-      "contracts/golos.token/golos.token.abi"
-    )
-
-    console.log("build contract")
-    await eosTest.make("contracts/golos.worker")
-
-    console.log("create app token")
-    await tokenContract.create(appName, `1000000000 ${tokenSymbol}`, {
-      authorization: "golos.token"
-    })
-    await tokenContract.issue(appName, `1000000000 ${tokenSymbol}`, {
-      authorization: appName
-    })
-
-    console.log("deploy golos.worker contract")
-    const contract = await eosTest.deploy(
-      "golos.worker",
-      "contracts/golos.worker/golos.worker.wasm",
-      "contracts/golos.worker/golos.worker.abi"
-    )
-
-    console.log("create")
-    await contract.createpool(appName, tokenSymbol, { authorization: appName })
-
-    console.log("Workers pool replenishment")
+    console.log("Workers pool replenishment");
     await tokenContract.transfer(
       appName,
       "golos.worker",
       `1000 ${tokenSymbol}`,
       appName /* memo */
-    )
+    );
     await tokenContract.transfer(
       appName,
       "golos.worker",
       `500 ${tokenSymbol}`,
       appName /* memo */
-    )
+    );
     let funds = await eosTest.api.getTableRows(
       true,
       "golos.worker",
       appName,
       "funds",
       appName
-    )
-    console.log("funds table:", funds)
-    expect(funds.rows.length).not.toEqual(0)
-    expect(funds.rows[0].quantity).toEqual(`1500 ${tokenSymbol}`)
+    );
+    console.log("funds table:", funds);
+    expect(funds.rows.length).not.toEqual(0);
+    expect(funds.rows[0].quantity).toEqual(`1500 ${tokenSymbol}`);
 
-    console.log("addpropos")
+    console.log("addpropos");
     const proposals = [
       {
         id: 0,
         title: "Proposal #1",
         text: "Let's create worker's pool",
-        user: "user1"
+        user: memberAccounts[0]
       },
       {
         id: 1,
         title: "Proposal #2",
         text: "Let's create golos fork",
-        user: "delegate1"
+        user: memberAccounts[1]
       }
-    ]
+    ];
 
     const comments = [
-      { id: 0, user: "delegate1", text: "Let's do it!" },
-      { id: 1, user: "delegate2", text: "Noooo!" }
-    ]
+      { id: 0, user: delegateAccounts[0], text: "Let's do it!" },
+      { id: 1, user: delegateAccounts[1], text: "Noooo!" }
+    ];
 
     const tspecs = [
       {
         id: 0,
-        author: "user1",
+        author: memberAccounts[2],
         text: "Technical specification #1",
         specification_cost: `100 ${tokenSymbol}`,
         specification_eta: Math.round(Date.now() / 1000 + 1000),
@@ -103,7 +121,7 @@ it(
       },
       {
         id: 1,
-        author: "user1",
+        author: memberAccounts[3],
         text: "Technical specification #2",
         specification_cost: `500.0 ${tokenSymbol}`,
         specification_eta: Math.round(Date.now() / 1000 + 1000),
@@ -111,10 +129,10 @@ it(
         development_eta: Math.round(Date.now() / 2000 + 30000),
         payments_count: 2
       }
-    ]
+    ];
 
     for (let proposal of proposals) {
-      console.log(proposal)
+      console.log(proposal);
       await contract.addpropos(
         appName,
         proposal.id,
@@ -122,16 +140,16 @@ it(
         proposal.title,
         proposal.text,
         { authorization: proposal.user }
-      )
-      await contract.votepropos(appName, proposal.id, "delegate1", 1, {
-        authorization: "delegate1"
-      })
-      await contract.votepropos(appName, proposal.id, "delegate2", 0, {
-        authorization: "delegate2"
-      })
+      );
+      await contract.votepropos(appName, proposal.id, delegateAccounts[0], 1, {
+        authorization: delegateAccounts[0]
+      });
+      await contract.votepropos(appName, proposal.id, delegateAccounts[1], 0, {
+        authorization: delegateAccounts[1]
+      });
 
       for (let comment of comments) {
-        console.log("addcomment", comment)
+        console.log("addcomment", comment);
         await contract.addcomment(
           appName,
           proposal.id,
@@ -139,11 +157,11 @@ it(
           comment.user,
           comment,
           { authorization: comment.user }
-        )
+        );
       }
 
       for (let tspec of tspecs) {
-        console.log("technical specification", tspec)
+        console.log("technical specification", tspec);
         await contract.addtspec(
           appName,
           proposal.id,
@@ -151,16 +169,16 @@ it(
           tspec.author,
           tspec,
           { authorization: tspec.author }
-        )
+        );
       }
     }
 
-    console.log("getting proposals table")
+    console.log("getting proposals table");
 
     console.log(
       "proposals table",
       await eosTest.api.getTableRows(true, "golos.worker", appName, "proposals")
-    )
+    );
 
     // for (let proposal of proposals) {
     //   for (let comment of comments) {
@@ -171,13 +189,13 @@ it(
     //   await contract.delpropos(appName, proposal.id, {authorization: proposal.user})
     // }
 
-    eosTest.destroy()
-    done()
+    eosTest.destroy();
+    done();
   },
   300000
-)
+);
 
 afterAll(async done => {
-  await eosTest.destroy()
-  done()
-})
+  await eosTest.destroy();
+  done();
+});
